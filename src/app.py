aff1d86adae2933d8f05b9c888add33e80f9634d
hayatokatagiri <zurqnd@gmail.com>
2024-07-04 15:14:59 +0900

    app.pyの修正

--- src/app.py
+++ src/app.py
@@ -3,7 +3,6 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')  # 必要に応じて、スクリプトの先頭でこの行を追加します
 
 # サーバーを立ち上げるにはターミナルで以下のコマンドを実行
 # streamlit run src/app.py
