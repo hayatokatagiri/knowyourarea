f2384aaa4c1b3ee75880f5fc3526f208c36ed3c1
hayatokatagiri <zurqnd@gmail.com>
2024-07-04 15:08:54 +0900

    deployしたあとのmatplotlibのエラーに対応

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 # import
+import numpy as np
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+matplotlib.use('Agg')  # 必要に応じて、スクリプトの先頭でこの行を追加します
 
 # サーバーを立ち上げるにはターミナルで以下のコマンドを実行
 # streamlit run src/app.py
